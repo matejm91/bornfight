0708a981edf3f0096bd22b32ffebe22eefc2e733
mmaric <matej.maric@sedamit.hr>
2020-12-12 20:55:22 +0100

    Change to functional component

--- src/components/common/AlbumListHeader.jsx
+++ src/components/common/AlbumListHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import 'src/assets/style/_albumListHeader.css';
-import AlbumListFilter from "src/components/album/list/AlbumListFilter";
+import AlbumListFilter from 'src/components/album/list/AlbumListFilter';
 
 const propTypes = {
   onFilterInputChange: PropTypes.func,
@@ -9,17 +9,19 @@ const propTypes = {
   suggestions: PropTypes.array,
 };
 
-class AlbumListHeader extends React.Component {
-  render () {
-    const {onFilterInputChange, onFilterSubmit, suggestions} = this.props;
+function AlbumListHeader(props) {
+  const { onFilterInputChange, onFilterSubmit, suggestions } = props;
 
-    return (
-      <div className='bornfight-albumListHeader__header'>
-        <p>Album List</p>
-        <AlbumListFilter onFilterInputChange={onFilterInputChange} onFilterSubmit={onFilterSubmit} suggestions={suggestions}/>
-      </div>
-    );
-  }
+  return (
+    <div className="bornfight-albumListHeader__header">
+      <p>Album List</p>
+      <AlbumListFilter
+        onFilterInputChange={onFilterInputChange}
+        onFilterSubmit={onFilterSubmit}
+        suggestions={suggestions}
+      />
+    </div>
+  );
 }
 
 AlbumListHeader.propTypes = propTypes;
